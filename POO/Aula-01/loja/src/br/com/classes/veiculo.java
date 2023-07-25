3e830d8f87930ef0edc927141e148f7b46577198
MatiasParizoti <matehusparizoti00@gmail.com>
2023-07-25 12:03:49 -0300

    AULA01

---
+++ POO/Aula-01/loja/src/br/com/classes/veiculo.java
@@ -0,0 +1,23 @@
+package br.com.classes;
+
+public class veiculo {
+	
+	//Atributo
+	public String nome;
+	public String cor;
+	public String rodas;
+	public String motor;
+	public float velocidade; 
+	
+	//Ações
+	public void ligar() {
+		
+		System.out.println("O veículo ligou ");
+	}
+	
+	public void desligar() {
+		System.out.println("O veículo desligou ");
+		
+	}
+
+}
